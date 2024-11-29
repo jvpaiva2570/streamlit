d236de7bc59f0a9271bc9c61dd997ce87687fe4b
jvpaiva2570 <jv.pl257@gmail.com>
2024-11-29 03:08:47 -0300

    Update calculos.py

--- calculos.py
+++ calculos.py
@@ -2,30 +2,21 @@ def calcular_tempo_total(qtd_servicos, tempo_por_servico):
     """Calcula o tempo total gasto em um tipo de serviço."""
     return qtd_servicos * tempo_por_servico
 
-
 def calcular_tempo_parado(dados_caminhao):
-    """Calcula o tempo total parado de um caminhão."""
-    tempo_preventiva_250h = 8
-    tempo_preventiva_500h = 12
-    tempo_preventiva_1000h = 16
-    tempo_preventiva_16000h = 168
-
-    tempo_parado_250h = dados_caminhao["qtd_250h"] * tempo_preventiva_250h
-    tempo_parado_500h = dados_caminhao["qtd_500h"] * tempo_preventiva_500h
-    tempo_parado_1000h = dados_caminhao["qtd_1000h"] * tempo_preventiva_1000h
-    tempo_parado_16000h = (
-        dados_caminhao["qtd_16000h"] * tempo_preventiva_16000h
-    )
-    tempo_total_parado = (
-        tempo_parado_250h
-        + tempo_parado_500h
-        + tempo_parado_1000h
-        + tempo_parado_16000h
-    )
-    tempo_corretivo = dados_caminhao["taxa_corretiva"] * tempo_total_parado
-    tempo_total_parado += tempo_corretivo
+    """Calcula o tempo total parado de um caminhão, incluindo corretivas."""
+    tempo_servicos = {
+        "qtd_250h": 8,
+        "qtd_500h": 12,
+        "qtd_1000h": 16,
+        "qtd_16000h": 168,
+    }
+    tempo_total_parado = 0
+    for servico, tempo_servico in tempo_servicos.items():
+        tempo_total_parado += dados_caminhao[servico] * tempo_servico
+    
+    # Tempo corretivo incluso diretamente
+    tempo_total_parado *= (1 + dados_caminhao["taxa_corretiva"])  
     return tempo_total_parado
-
 
 def calcular_df(tempo_total_parado):
     """Calcula a disponibilidade física (DF) de um caminhão."""
@@ -36,11 +27,10 @@ def calcular_df(tempo_total_parado):
     ) * 100
     return df
 
-
 def calcular_utilizacao(dados_caminhao):
     """
     Calcula a utilização, a hora não utilizada e as horas trabalhadas de um caminhão.
-    
+
     Args:
       dados_caminhao: Dicionário com os dados do caminhão.
 
@@ -50,7 +40,8 @@ def calcular_utilizacao(dados_caminhao):
 
     dias_programados = 365  # Informação fixa da planilha
     horas_programadas_dia = 24  # Informação fixa da planilha
-    tempo_troca_turno = 0.5  # Informação fixa da planilha
+    qtd_orientacoes_gerenciais = dias_programados * 3  # Quantidade de orientações gerenciais
+    tempo_orientacao_gerencial = 0.08  # Tempo perdido por orientação gerencial (em horas)
 
     tempo_250h = calcular_tempo_total(dados_caminhao["qtd_250h"], 8)
     tempo_500h = calcular_tempo_total(dados_caminhao["qtd_500h"], 12)
@@ -61,12 +52,19 @@ def calcular_utilizacao(dados_caminhao):
     # calculado com base na taxa corretiva
     tempo_total_parado = calcular_tempo_parado(dados_caminhao)
 
-    # Tempo das novas paradas 
+        # Calcula as horas disponíveis
+    df = calcular_df(tempo_total_parado)
+    horas_disponiveis = dias_programados * horas_programadas_dia * (df/100)  # Divide a DF por 100 para obter o valor decimal
+
+    # Tempo das novas paradas
     tempo_sem_operador = calcular_tempo_total(dados_caminhao["qtd_sem_operador"], 1)
     tempo_parada_desmonte = calcular_tempo_total(dados_caminhao["qtd_parada_desmonte"], 2)
     tempo_parada_climatica = calcular_tempo_total(dados_caminhao["qtd_parada_climatica"], 1)
-    tempo_almoco = calcular_tempo_total(dados_caminhao["qtd_almoco"], 1)  # 1 hora de almoço
-    tempo_troca_turno = calcular_tempo_total(dados_caminhao["qtd_troca_turno"], 0.08)  # 0.5 horas por troca de turno
+    tempo_almoco = calcular_tempo_total(dados_caminhao["qtd_almoco"], 1)*365  # 1 hora de almoço
+    tempo_troca_turno = calcular_tempo_total(dados_caminhao["qtd_troca_turno"], 0.08)*365  # 0.08 horas por troca de turno
+    tempo_orientacao_gerencial = calcular_tempo_total(qtd_orientacoes_gerenciais, tempo_orientacao_gerencial) # Calcula o tempo perdido com orientação gerencial
+    tempo_absenteismo = ((dados_caminhao["perc_absenteismo"] / 100) *horas_programadas_dia)*dias_programados
+    tempo_treinamento = ((dados_caminhao["perc_treinamento"] / 100) * horas_programadas_dia)*dias_programados
 
     tempo_total_parado += (
         tempo_sem_operador
@@ -74,49 +72,72 @@ def calcular_utilizacao(dados_caminhao):
         + tempo_parada_climatica
         + tempo_almoco
         + tempo_troca_turno
+        + tempo_orientacao_gerencial
+        + tempo_absenteismo
+        + tempo_treinamento
+
     )
 
-    horas_disponiveis = (
-        dias_programados * horas_programadas_dia
-    ) - tempo_total_parado
+    
 
-    # Calcula a hora não utilizada
-    horas_nao_utilizadas = calcular_tempo_total(dados_caminhao["qtd_hnu"], 8)
+
+    
+
+
+    # Calcula o tempo perdido em cada operação
+    tempo_perdido = calcular_tempo_perdido(dados_caminhao)
+
+    # Calcula a hora não utilizada (HNU) como a soma dos tempos perdidos
+    horas_nao_utilizadas = sum(tempo_perdido.values())
 
     # Calcula as horas trabalhadas
     horas_trabalhadas = horas_disponiveis - horas_nao_utilizadas
 
     # Calcula a utilização
     if horas_disponiveis == 0:
-        return 0, horas_nao_utilizadas, horas_trabalhadas  # Retorna a utilização, as horas não utilizadas e as horas trabalhadas
+        return 0, horas_nao_utilizadas, horas_trabalhadas
 
     utilizacao = (horas_trabalhadas / horas_disponiveis) * 100
 
-    # Calcula o tempo de absenteísmo e treinamento (em horas)
-    tempo_absenteismo = (dados_caminhao["perc_absenteismo"] / 100) * horas_trabalhadas
-    tempo_treinamento = (dados_caminhao["perc_treinamento"] / 100) * horas_trabalhadas
-
-    return utilizacao, horas_nao_utilizadas, horas_trabalhadas
 
 
-def calcular_tempo_perdido(dados_caminhao):
+
+    return utilizacao, horas_nao_utilizadas, horas_trabalhadas, horas_disponiveis
+
+def calcular_tempo_perdido(dados_caminhao):  # Adicione horas_trabalhadas como argumento
     """Calcula o tempo perdido em cada operação."""
 
     tempo_perdido = {}
 
-    # Tempo perdido por falta de operador (considerando 8 horas por parada)
-    tempo_perdido["Sem Operador"] = calcular_tempo_total(dados_caminhao["qtd_sem_operador"], 8)
+    # Tempo perdido por falta de operador (considerando 1 hora por parada)
+    tempo_perdido["Sem Operador"] = calcular_tempo_total(dados_caminhao["qtd_sem_operador"], 1) 
 
-    # Tempo perdido por parada para desmonte (considerando 8 horas por parada)
-    tempo_perdido["Parada Desmonte"] = calcular_tempo_total(dados_caminhao["qtd_parada_desmonte"], 8)
+    # Tempo perdido por parada para desmonte (considerando 2 horas por parada)
+    tempo_perdido["Parada Desmonte"] = calcular_tempo_total(dados_caminhao["qtd_parada_desmonte"], 2) 
 
-    # Tempo perdido por parada climática (considerando 8 horas por parada)
-    tempo_perdido["Parada Climática"] = calcular_tempo_total(dados_caminhao["qtd_parada_climatica"], 8)
+    # Tempo perdido por parada climática (considerando 1 hora por parada)
+    tempo_perdido["Parada Climática"] = calcular_tempo_total(dados_caminhao["qtd_parada_climatica"], 1) 
 
     # Tempo perdido com almoço (considerando 1 hora por almoço)
-    tempo_perdido["Almoço"] = calcular_tempo_total(dados_caminhao["qtd_almoco"], 1)
+    tempo_perdido["Almoço"] = calcular_tempo_total(dados_caminhao["qtd_almoco"], 1) * 365
 
-    # Tempo perdido com troca de turno (considerando 0.5 horas por troca)
-    tempo_perdido["Troca de Turno"] = calcular_tempo_total(dados_caminhao["qtd_troca_turno"], 0.5)
+    # Tempo perdido com troca de turno (considerando 0.08 horas por troca)
+    tempo_perdido["Troca de Turno"] = calcular_tempo_total(dados_caminhao["qtd_troca_turno"], 0.08) * 365
+
+    # Tempo perdido com orientação gerencial
+    dias_programados = 365
+    horas_dia=24
+    
+    qtd_orientacoes_gerenciais = dias_programados * 3
+    tempo_orientacao_gerencial = 0.08
+    tempo_perdido["Orientação Gerencial"] = calcular_tempo_total(
+        qtd_orientacoes_gerenciais, tempo_orientacao_gerencial
+    )
+
+    # Tempo perdido com absenteísmo (em horas)
+    tempo_perdido["Absenteísmo"] = ((dados_caminhao["perc_absenteismo"] / 100) * horas_dia)*dias_programados
+
+    # Tempo perdido com treinamento (em horas)
+    tempo_perdido["Treinamento"] = ((dados_caminhao["perc_treinamento"] / 100) * horas_dia)*dias_programados
 
     return tempo_perdido
