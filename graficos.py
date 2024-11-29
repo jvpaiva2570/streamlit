f443f98d8df906681ff6b9b480b4b7c948a4a440
jvpaiva2570 <jv.pl257@gmail.com>
2024-11-29 10:03:26 -0300

    Update graficos.py

--- graficos.py
+++ graficos.py
@@ -19,3 +19,23 @@ def gerar_grafico(caminhoes, dfs_caminhoes):
     ax.tick_params(axis="x", colors="black", rotation=45, labelsize=5)
     ax.tick_params(axis="y", colors="black", labelsize=5)
     st.pyplot(fig)
+
+def gerar_grafico_df_utilizacao(caminhoes, dfs, utilizacoes):
+  """Gera um gráfico de barras comparando a DF com a Utilização."""
+
+  fig, ax = plt.subplots(figsize=(10, 4))
+  bar_width = 0.35
+  index = np.arange(len(caminhoes))
+
+  barras_df = ax.bar(index, dfs, bar_width, label="DF", color='skyblue')
+  barras_utilizacao = ax.bar(index + bar_width, utilizacoes, bar_width, label="Utilização", color='lightcoral')
+
+  ax.set_xlabel("Caminhões", fontsize=12)
+  ax.set_ylabel("Percentual (%)", fontsize=12)
+  ax.set_title("Comparativo DF x Utilização", fontsize=14)
+  ax.set_xticks(index + bar_width / 2)
+  ax.set_xticklabels(caminhoes)
+  ax.legend()
+
+  plt.xticks(rotation=45)
+  st.pyplot(fig)
